23c88012862ad85c19f726acd8007d9abaa5d4c0
Ardhra <ardhra367617@gmail.com>
2024-08-21 21:55:06 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+document.write("Hello World");
+document.write("<br>");
+document.write("Hello World");
+console.log("This is a test message");
+console.warn("Warning");
+console.error("error message");
+
+//Single line comment
+/*this
+is a
+multiline comment */
+
+//let var const
+
+let person="Anie"
+person="Anie"
+console.log(person)
+
+var age=19
+console.log(age)
+
+const pi=3.14
+console.log(pi)
+
+console.log(a)
+var a=10
+
+// console.log(b)
+// let b=10
+//Hoisting
+
+
+
+
+
+
